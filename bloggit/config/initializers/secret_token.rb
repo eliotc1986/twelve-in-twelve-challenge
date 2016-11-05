b4d8bb174020fb48786505876272a0366d8ee597
Elliott Hindman <elliott_hindman@metaps.com>
2016-11-05 22:57:02 +0900

    add bloggit repo

---
+++ bloggit/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bloggit::Application.config.secret_key_base = 'fb9977f4071d77147c64d930495f87d1606db43c40925f8c3bbd6be866999e55199dd338b7a546acfdc908ca39c9bbd76e5d5a9b26a1dcb1411f5764d6c0bc3d'
