c6c77bf4ed1867575812b0b1e0eca0ea6fafe9c2
Elliott Hindman <elliott_hindman@metaps.com>
2016-11-13 10:40:25 +0900

    add movie review

---
+++ movie-review/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MovieReview::Application.config.secret_key_base = '958a562b600124a6f1a27ef83688d7d886a422bf70215491a781e8d35e8b92a9b9c14eb3ded29f11c47277506d022152e4e43e86b1a46fc888b65821f63297b1'
